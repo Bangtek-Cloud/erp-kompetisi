d67ffc0b5e9b0a0afecdcfa17176dd0c61bfe44a
kepsgurih <kepsgurih@gmail.com>
2025-03-19 23:40:17 +0700

    :sparkles: init

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -18,7 +18,7 @@ export const HeroSection = () => {
 
                     <div className="max-w-screen-md mx-auto text-center text-4xl md:text-6xl font-bold">
                         <h1>
-                            Alami
+                            Mulai <br/>
                             <span className="text-transparent px-2 bg-gradient-to-r from-[#D247BF] to-primary bg-clip-text">
                                 Kompetisi
                             </span>
