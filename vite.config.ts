0c8bfc852301dc705d7134315bb5017311143cd3
kepsgurih <kepsgurih@pm.me>
2026-01-11 22:13:16 +0700

    :wrench: clean ui

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
    server: {
     proxy: {
       "/api": {
-        target: "https://api.bangtek.org/",
+        target: "http://localhost:3000/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/ep/, ''),
       }
